6e7aa9a0ee0a8225e46e6f98097dbda98770ca20
Jermanus <133773488+jermanus74@users.noreply.github.com>
2024-12-10 23:29:35 +0300

    modified login and sign up to check if credentials are valid so as to continue..other wise user return to required field

--- main.cpp
+++ main.cpp
@@ -4,114 +4,188 @@
 #include <string>
 using namespace std;
 
-struct  userDb{
+struct userDb {
     string full_name;
     string id;
     string password;
-    string verify_password;
     string status;
-    void display()const{
-        cout<<"Username: "<<full_name<<endl<<"ID:"<<id<<endl<<"Status"<<status<<endl;
+
+    void display() const {
+        cout << "Username: " << full_name << endl
+             << "ID: " << id << endl
+             << "Status: " << status << endl;
     }
 };
 
-const string UserProfile="userDb.txt";
-vector <userDb> loadUser();
+const string UserProfile = "userDb.txt";
+vector<userDb> userList;
+
+// Function Prototypes
+vector<userDb> loadUser();
 void signUp();
 void login();
 void EditUserDetails();
 void ReadUserDetails();
-void saveUser(const vector<userDb> &user);
-vector <userDb> userList=loadUser();
+void saveUser(const vector<userDb> &users);
 void home();
 
-int main()
-{
+int main() {
+    userList = loadUser(); // Initialize user list from file
+
+    int choice;
+    do {
+        cout << "\n--- User Management System ---" << endl;
+        cout << "1. Sign Up" << endl;
+        cout << "2. Log In" << endl;
+        cout << "3. View All Users" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(); // Clear input buffer
+
+        switch (choice) {
+            case 1:
+                signUp();
+                break;
+            case 2:
+                login();
+                break;
+            case 3:
+                ReadUserDetails();
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
+    } while (choice != 4);
 
     return 0;
-};
-void home(){
-    cout<<"Not yet implemented..."<<endl;
 }
-void signUp(){
-    userDb Account;
-    cout<<"Enter username: "<<endl;
-    getline(cin,Account.full_name);
-    cout<<"Enter Id: "<<endl;
-    getline(cin,Account.id);
-//    a mechanism which checks if user id exist in db
-    cout<<"Set account password: "<<endl;
-    getline(cin,Account.password);
-    cout<<"Confirm account password: "<<endl;
-    getline(cin,Account.verify_password);
-    if (Account.password!=Account.verify_password){
-        cout<<"Password don't match... Re-enter your password to confirm."<<endl;
 
-    }else{
-        userList.push_back(Account);
-        saveUser(userList);
-    }
+void home() {
+    cout << "Welcome to the home page! (Not yet implemented)" << endl;
 }
-void login(){
+
+void signUp() {
     userDb Account;
-    string ID,password;
-    cout<<"Enter your id: "<<endl;
-    getline(cin,ID);
-    for (const auto &user:userList) {
-        if (ID==Account.id){
-            cout<<"Enter your password: "<<endl;
-            getline(cin,password);
-            if (password==Account.password){
-                cout<<"User successfully logged in... Redirecting to home page"<<endl;
-                home();
-            }
-            else{
-                cout<<"Invalid password... Try again"<<endl;
+    bool idExists;
+
+    cout << "Enter username: ";
+    getline(cin, Account.full_name);
+
+    do {
+        idExists = false;
+        cout << "Enter ID: ";
+        getline(cin, Account.id);
+
+        // Check if ID already exists
+        for (const auto &user : userList) {
+            if (user.id == Account.id) {
+                idExists = true;
+                cout << "ID already exists. Please try again with a different ID." << endl;
+                break;
             }
         }
-        else{
-            cout<<"User ID not found... Check your ID and try again"<<endl;
-        }
-    }
+    } while (idExists);
 
+    cout << "Set account password: ";
+    getline(cin, Account.password);
+
+    string verify_password;
+    do {
+        cout << "Confirm account password: ";
+        getline(cin, verify_password);
+
+        if (Account.password != verify_password) {
+            cout << "Passwords don't match. Please try again." << endl;
+        }
+    } while (Account.password != verify_password);
+
+    Account.status = "Active"; // Default status
+    userList.push_back(Account);
+    saveUser(userList);
+    cout << "Account created successfully!" << endl;
 }
-vector <userDb> loadUser(){
+
+void login() {
+    string ID, password;
+    bool loggedIn = false;
+
+    do {
+        cout << "Enter your ID: ";
+        getline(cin, ID);
+
+        bool userFound = false;
+        for (const auto &user : userList) {
+            if (user.id == ID) {
+                userFound = true;
+
+                cout << "Enter your password: ";
+                getline(cin, password);
+
+                if (user.password == password) {
+                    cout << "Login successful! Redirecting to home page..." << endl;
+                    home();
+                    loggedIn = true;
+                } else {
+                    cout << "Invalid password. Try again." << endl;
+                }
+                break;
+            }
+        }
+
+        if (!userFound) {
+            cout << "User ID not found. Please try again." << endl;
+        }
+    } while (!loggedIn);
+}
+
+vector<userDb> loadUser() {
     vector<userDb> users;
-    ifstream file(userList);
-    if (file.is_open()){
+    ifstream file(UserProfile);
+
+    if (file.is_open()) {
         userDb Account;
-        while(getline(cin,Account.id)){
-            getline(cin,Account.full_name);
-            getline(cin,Account.password);
+        while (getline(file, Account.full_name)) {
+            getline(file, Account.id);
+            getline(file, Account.password);
+            getline(file, Account.status);
+            users.push_back(Account);
         }
         file.close();
+    } else {
+        cout << "No existing user database found. Starting fresh." << endl;
     }
+
     return users;
 }
-void saveUser(const vector<userDb> &user){
-    ofstream  file(userList,ios::trunc);
-    if (file.is_open()){
-        for (const auto &s:user) {
-            file<<"Username: "<<s.full_name<<endl
-            <<"ID: "<<s.id<<endl
-            <<"Password: "<<s.password<<endl;
+
+void saveUser(const vector<userDb> &users) {
+    ofstream file(UserProfile, ios::trunc);
+
+    if (file.is_open()) {
+        for (const auto &user : users) {
+            file << user.full_name << endl
+                 << user.id << endl
+                 << user.password << endl
+                 << user.status << endl;
         }
-    }
-    else{
-        cout<<"failed to open file..... Try again"<<endl;
+        file.close();
+    } else {
+        cout << "Failed to open file for saving. Please try again." << endl;
     }
 }
-void ReadUserDetails(){
-    if (userList.empty()){
-        cout<<"No user found... Try to sign up"<<endl;
-    }else{
-        cout<<"----- User List -------"
-        for (const auto &user: userList) {
+
+void ReadUserDetails() {
+    if (userList.empty()) {
+        cout << "No users found. Please sign up to add users." << endl;
+    } else {
+        cout << "\n----- User List -----" << endl;
+        for (const auto &user : userList) {
             user.display();
+            cout << "-------------------" << endl;
         }
     }
-
 }
-
-
-
